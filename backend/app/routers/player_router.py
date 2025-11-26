376e7837e425a98b5b7c92b8015ad77ee15f2871
Voppor41 <egorselifanov334@yandex.com>
2025-11-26 23:17:02 +0300

    Finished setting user routers

--- backend/app/routers/player_router.py
+++ backend/app/routers/player_router.py
@@ -29,7 +29,7 @@ async def create_player(player_data: PlayerCreate, user_service: UserService = D
         logger.error(f"Error creating player: {e}")
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Iternal server error")
 
-@router.get("{/player_id", response_model=PlayerResponse)
+@router.get("/{player_id", response_model=PlayerResponse)
 async def get_player(player_id: int, user_service: UserService = Depends(get_user_service)):
 
     player = await user_service.get_player_by_id(player_id)
@@ -38,3 +38,29 @@ async def get_player(player_id: int, user_service: UserService = Depends(get_use
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
 
     return player
+
+@router.put("/{player_id}/goals", response_model=PlayerResponse)
+async def update_player_goals(player_id: int,
+                              goals_update: PlayerUpdate,
+                              user_service: UserService = Depends(get_user_service)):
+
+    try:
+        player = await user_service.update_player_goals(player_id, goals_update.goals)
+        return player
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
+@router.get("/{player_id}/quest")
+async def get_player_quest(player_id: int,
+                           completed: bool = None,
+                           user_service: UserService = Depends(get_user_service)):
+
+    try:
+        quest = await user_service.get_player_quest(player_id, completed)
+        return {"quest": quest, "count": len(quest)}
+
+    except Exception as e:
+        logger.error(f"Error founding player quest: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Iternal server error")
+    
